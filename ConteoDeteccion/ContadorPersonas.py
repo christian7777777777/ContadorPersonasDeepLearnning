6da878a1d3819ac5d51e94d65093b15825d883cd
christian7777777777 <150096015+Manuman1505@users.noreply.github.com>
2024-02-06 22:27:48 -0500

    ContadorDetector_v2.7

--- ConteoDeteccion/ContadorPersonas.py
+++ ConteoDeteccion/ContadorPersonas.py
@@ -1,15 +1,17 @@
-import tkinter as tk
-import cv2
-import torch
-import numpy as np 
-import matplotlib.path as mplPath
-from sort import Sort
-import imutils
-from PIL import Image, ImageTk
+import tkinter as tk                #Libreria para GUI
+import cv2                          #Libreria para captura de Video
+import torch                        #Libreria para carga y desplige de Modelo YOLO
+import numpy as np                  #Libreria para manejo de matrices
+import matplotlib.path as mplPath   #Libreria para graficas
+from sort import Sort               #Libreria para Tracking
+import imutils                      #Librerias para redimencionar el video
+from PIL import Image, ImageTk      #Libreria para convertir frame en Imagen
 
 class ContadorPersonas():
 
     def __init__(self):
+        
+        #Constructor con variables de clase ContadorPersonas(), Zonas de deteccion predefinidas y lista de Detecciones (Vacia)
         self.zonas = [
             np.array([[3,236],[3,184],[298,212],[288,291]]),
             np.array([[532,184],[692,222],[762,150],[762,97],[727,93]]),
@@ -20,73 +22,96 @@ class ContadorPersonas():
             np.array([[4,76],[2,136],[240,86],[297,70],[278,43],[207,38]])
             ]
         self.ListaDetecciones = []
-   
+    
+    #Metodo para cargar modelo de Deteccion (YOLO)
     def load_model(self):
         model = torch.hub.load("ultralytics/yolov5", model="yolov5n", pretrained=True)
         return model
 
+    #Metodo para obtener los Bbox de cada deteccion
     def get_bdoxes(self, preds: object):
         df = preds.pandas().xyxy[0]
-        df = df[df["confidence"] >= 0.5]
-        df = df[df["name"] == "person"]
+        df = df[df["confidence"] >= 0.5]    #Establecemos un nivel de confianza de deteccion de 50%
+        df = df[df["name"] == "person"]     #Filtramos detecciones a personas
 
-        return df[["xmin", "ymin", "xmax", "ymax"]].values.astype(int)
+        return df[["xmin", "ymin", "xmax", "ymax"]].values.astype(int)  #Retorna bbox de detecciones
 
+    #Metodo para Obtener el centro del Bbox de deteccion que ayudara en el conteo de personas por zona
     def get_center(self, bbox):
         #xmin, ymin, xmax, ymax
         # 0     1     2     3
         center = ((bbox[0] + bbox[2]) // 2, (bbox[1] + bbox[3]) // 2)
-        return center
+        return center   #retorna tupla (xc, yc)
 
+    #Metodo para obtener el centro de zona para poder mostrar metrias de conteo
     def get_center_zona(self, poligon):
 
         center = (int(np.mean(poligon[:, 0])-50), int(np.mean(poligon[:, 1])))
-        return center
+        return center   #retorna tupla (xc, yc)
 
+    #Metodo para detectar si el centro de un Bbox Detectado estan dentro de una zona(poligono)
     def is_valid_detection(self, xc, yc, zona):
+        #Pasamos como parametros, el centro del Bbox de personas detectada y el Poligino de Zona
         return mplPath.Path(zona).contains_point((xc, yc))
     
+    #Metodo para Deteccion y conteo de Personas en zonas determinadas
     def detector(self, cap: object, lblVideo: tk, lblDetecciones: list):
+        #Parametros:
+        #   Cap -> VideoCaputure()
+        #   lblVideo -> Label para mostrar frame por frame en GUI 
+        #   lblDetecciones -> lista de StringVar a ser modificadas con metricas de conteo 
   
-        model = self.load_model()
-        tracker = Sort()
+        model = self.load_model()   #Cargar modelo
+        tracker = Sort()            #Utilizacion de Sort() para seguimiento de Bbox (Tracking)
 
+        #Ciclo de Frames del Video
         while cap.isOpened():
             ret, frame = cap.read()
 
             if not ret:
                 break
 
+            #Predicciones con Modelo Cargado
             preds = model(frame)
+
+            #Utilizacion Metodo para obtener Bbox de Detecciones
             bboxes = self.get_bdoxes(preds)
             
+            #Sort solo necesita Bbox para realizar el Tracking del Objeto y me retorna su id
             tracks = tracker.update(bboxes)
-            tracks = tracks.astype(int)
+            tracks = tracks.astype(int)     #[xmin, xmax, ymin, ymax, id]
 
+
+            #Variable auxiliar para redicciones de cada frame
             deteccion_zona =[0, 0, 0, 0, 0, 0, 0]
 
+            #Ciclo para contar las personas que estan en cada zona con la variable "tracks", es decir con su Id de seguimiento por Sort()
             for box in tracks:
                 xc, yc = self.get_center(box)
                 for i, zona in enumerate(self.zonas, start=0):
                     if self.is_valid_detection(xc, yc, zona):
                         deteccion_zona[i] += 1
 
-                cv2.circle(img=frame, center=(xc, yc), radius=5, color=(0,255,0), thickness=-1)
+                #Dibujado de detecciones en el frame, asi como su Bbox y Id. 
+                cv2.circle(img=frame, center=(xc, yc), radius=5, color=(0,255,0), thickness=-1) #ciculo verde en el centro del Bbox
                 cv2.putText(img=frame, text=f"Id: {box[4]}", org=(box[0], box[1]-10), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(0,255,0), thickness=2)
                 cv2.rectangle(img=frame, pt1=(box[0], box[1]),pt2=(box[2], box[3]), color=(255, 0, 0), thickness=2)
 
+            #Ciclo para debujar cada zona (Poligono) Pre-Establecida
             for i, zona in enumerate(self.zonas, start=1):
                 x_center, y_center = self.get_center_zona(zona)
                 cv2.polylines(img=frame, pts=[zona], isClosed=True, color=(0, 0, 255), thickness=2)
-                cv2.putText(img=frame, text=f"Zona{i}: {deteccion_zona[i-1]}", org=(x_center, y_center), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(255, 255, 255), thickness=2)
-                lblDetecciones[i-1].set(f"Personas en Zona {i}: {deteccion_zona[i-1]} Personas")
+                cv2.putText(img=frame, text=f"Zona{i}: {deteccion_zona[i-1]}", org=(x_center, y_center), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(255, 255, 255), thickness=2)  #Metricas de conteo en Frame
+                lblDetecciones[i-1].set(f"Personas en Zona {i}: {deteccion_zona[i-1]} Personas")    #Metricas de conteo en labels GUI
 
+            #Agregacion de Lista de detecciones de cada frame a una lista de detecciones en todos los frames que van mostrandose
+            #Esto ayudara a realizar las graficas al final del video
             self.ListaDetecciones.append(deteccion_zona)
 
-            # Rendimensionamos el video
+            #Rendimensionamos el video
             frame = imutils.resize(frame, width=640)
 
-            # Convertimos el video
+            #Convertimos el video
             im = Image.fromarray(frame)
             img = ImageTk.PhotoImage(image=im)
 
@@ -94,36 +119,48 @@ class ContadorPersonas():
             lblVideo.configure(image=img)
             lblVideo.image = img
 
+            #Condicional para romper el ciclo de frames
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
-
+        
+        #Metodo para eliminar filas de recgistros de detecciones repetidas
+        #   ***IMPORTANTE***
+        #   Esto solo es importante para graficar los maximos de detecciones al final del video
         filas_unicas = []
         for fila in self.ListaDetecciones:
             if fila not in filas_unicas:
                 filas_unicas.append(fila)
-
+        
+        #Convertir Lista de Listas en Matrices para mejor uso de Datos
         self.MatrizDetecciones = np.array(filas_unicas) #Matriz SIN filas repetidas
-        self.MatrizDeteccionesCompleta = np.array(self.ListaDetecciones) #Matriz Completa de detecciones en cada Frame
+        self.MatrizDeteccionesCompleta = np.array(self.ListaDetecciones) #Matriz Completa de detecciones en cada Frame (Esta la utlizamos para mostear graficas por zonas)
+        
+        #Reset de variables y Obtencion de Metodo get_stadistics() Para obtener los maximos de personas por zonas
         self.ListaDetecciones = []
         self.get_stadistics()
 
         cap.release()
 
+    #Metodo para obtener Maximos de Personas por zonas
     def get_stadistics(self):
+        
+        #Variables Auxiliares
         MaximoDetecciones = []
         row, column = self.MatrizDetecciones.shape
+        
+        #ciclo para obtener los maximos de cada columna de la matriz generada anteriormente 
         for j in range(column):
             aux = 0
             for i in range(row):
                 if (self.MatrizDetecciones[i][j] > aux):
                     aux = self.MatrizDetecciones[i][j]
             MaximoDetecciones.append(aux)
-            #print("Zona: ", j+1, " Se ha detectado un Maximo de: ", aux, "Personas")
-        return MaximoDetecciones
+        return MaximoDetecciones    #Retormanos Vector de Maximos de Personas por cada Zona
     
+    #Metodo para Obtener Vector de Detecciones de zona pasada por parametro (zone)
     def get_stadistics_zone(self, zone):
         columna_seleccionada = self.MatrizDeteccionesCompleta[:, zone]
         vector_x = np.arange(1, self.MatrizDeteccionesCompleta.shape[0] + 1)
         vector_y = columna_seleccionada
-        return vector_x, vector_y
+        return vector_x, vector_y   #Retorna Vector de Frames(vector_x) y vector de Detecciones en cada frame(vector_y) de zona especificada (zone)
 
--- ConteoDeteccion/GUI.py
+++ ConteoDeteccion/GUI.py
@@ -1,36 +1,42 @@
-import tkinter as tk
-from tkinter import ttk
-import threading
-import cv2
-from ContadorPersonas import ContadorPersonas
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.cm as cm
-import numpy as np
+import tkinter as tk                                                #Libreria para GUI
+from tkinter import ttk                                             #Libreria para GUI
+import threading                                                    #Libreria para ejecutar multiple Hilos en un mismo CPU
+import cv2                                                          #Libreria para captura de Video
+from ContadorPersonas import ContadorPersonas                       #Importacion de Metodo ContadorPersonas() de clase ContadorPersonas
+import matplotlib.pyplot as plt                                     #Libreria para Graficas
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg     #Libreria para Plot en GUI   
 
 class PersonDetectionApp:
+
     def __init__(self, root):
+        #Parametros de GUI
         self.root = root
         self.root.title("Detección y conteo de personas")
-        #self.root.geometry("1280x1000")
 
+        #Lectura del video e instanciacion de la clase "ContadorPersonas()"
         self.cap = cv2.VideoCapture(r"C:\Users\User\OneDrive\Escritorio\8tavo Semestre\Deep Learnning\Proyecto\ContadorPersonasDeepLearnning\Video\video.webm")
         self.contador = ContadorPersonas()
 
+        #Frame para el despliegue de Widgets de GUI
         self.main_frame = tk.Frame(self.root, border=5, borderwidth=5)
         self.main_frame.grid(row=0, column=0, columnspan=8, rowspan=13)
 
+        #Metodo para crear los widgets en Framne de GUI
         self.create_widgets()
 
     def create_widgets(self):
+
+        #Creacion de Botones para Iniciar y Detener proceso de "Deteccion y conteo de Personas"
         self.start_button = tk.Button(self.main_frame, text="Iniciar", command=self.start_detection, border=5, background="#B4C77F", foreground="black", borderwidth=5, font=("Times", 12), width=10)
         self.start_button.grid(row=0, column=0)
-
         self.stop_button = tk.Button(self.main_frame, text="Detener", command=self.stop_detection, state=tk.DISABLED, border=5, background="#FF8383", foreground="black", borderwidth=5, font=("Times", 12), width=10)
         self.stop_button.grid(row=0, column=1)
 
+        #Varibles(listas) para almacenar widgets(Etiquetas y StringVar)
         self.zone_labels = []
         self.zone_labels_text = []
+
+        #Ciclo para crear Labels de caza zona donde se mostraran metricas de conteo
         for i in range(7):
             label_text = tk.StringVar() 
             label_text.set(f"Personas en Zona {i+1}: 0 Personas")
@@ -39,9 +45,11 @@ class PersonDetectionApp:
             self.zone_labels.append(zone_label)
             self.zone_labels_text.append(label_text)
 
+        #Creacion de Boton para Graficar Datos de Aglomeracion global y por zonas
         self.show_graph_button = tk.Button(self.main_frame, text="Graficar", command=self.show_graphs, state=tk.DISABLED, border=5, background="#83C8FF", foreground="black", borderwidth=5, font=("Times", 12), width=5)
         self.show_graph_button.grid(row=9, column=1)
 
+        #Creacion de ComboBox para desplegar opciones de Graficas
         valores = ["Aglomeracion Maxima Por zonas", 
             "Aglomercacion Zona 1",
             "Aglomercacion Zona 2",
@@ -50,25 +58,28 @@ class PersonDetectionApp:
             "Aglomercacion Zona 5",
             "Aglomercacion Zona 6",
             "Aglomercacion Zona 7"]
-        
         self.Tipo_Grafica = ttk.Combobox(self.main_frame, values=valores, background="#AFC8DB", foreground="black", width=15, font=("Times", 12))
         self.Tipo_Grafica.current(0)
         self.Tipo_Grafica.grid(row=9, column=0)
-        
-        # Gráficas
+
+        #Creacion de figura y Axis para el Graficado de los datos
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.main_frame)
         self.canvas.get_tk_widget().grid(row=9, column=2, rowspan=4, columnspan=6)
 
+        #Creaion del Label para mostrar el video frame a frame dentro de GUI
         self.label_video = ttk.Label(self.main_frame, text="VIDEO")
         self.label_video.grid(row=0, column=2, rowspan=8, columnspan=6)
 
+    #Metodo de Inicio de Proceso(Deteccion y conteo de Personas)
     def start_detection(self):
         self.start_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.NORMAL)
 
+        #Creacion de Hilo para optimizar el proceso
         threading.Thread(target=self.detect_people).start()
 
+    #Metodo para detener el proceso de deteccion
     def stop_detection(self):
         self.start_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.DISABLED)
@@ -76,15 +87,28 @@ class PersonDetectionApp:
 
         self.cap.release()
 
+    #Metodo para utilizar la funcion (detector() de la clase Instanciada)
     def detect_people(self):
+
+        #Pasamos los parametros que necesita el metodo detector como:
+        #   cap -> VideoCapure()
+        #   Label_video -> Label donde se mostrara el video
+        #   zona_label_text -> StringVar para ser modificadas con metricas de conteo
         self.contador.detector(self.cap, self.label_video, self.zone_labels_text)
+        
+        #Obtencion de Cantidad Maxima de Personas detectadas en cada Zona
         self.maximos = self.contador.get_stadistics()
 
+    #Metodo para graficar Datos obtenidos
     def show_graphs(self):
+
+        #Limpiar Axis de la grafica anterior
         self.ax.clear()
 
+        #Obtencion de Seleccion de Tipo de Grafica del ComboBox
         tipoGrafica = self.Tipo_Grafica.get()
 
+        #Condicional para Dibujar en el Axis el tipo de grafica seleccionado
         if tipoGrafica == "Aglomeracion Maxima Por zonas":
             x = ["Zona 1", "Zona 2", "Zona 3", "Zona 4", "Zona 5", "Zona 6", "Zona 7"]
             y = self.maximos
@@ -93,7 +117,6 @@ class PersonDetectionApp:
             self.ax.grid(True)
             self.ax.set_xlabel("Zonas")
             self.ax.set_ylabel("Máximo de personas")
-
         elif tipoGrafica == "Aglomercacion Zona 1":
             x, y = self.contador.get_stadistics_zone(0)
             self.ax.set_title("Gráfica Aglomeración Zona 1")
@@ -144,6 +167,7 @@ class PersonDetectionApp:
             self.ax.set_xlabel("Frame")
             self.ax.set_ylabel("Personas por Frame")
         
+        #Despliege de la grafica
         self.canvas.draw()
 
 if __name__ == "__main__":
